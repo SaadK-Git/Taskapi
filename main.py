335526595494afa2b2e6aabc29c1cb7142c6e800
studentforlife123 <119174122+studentforlife123@users.noreply.github.com>
2026-04-17 08:03:04 +0530

    Updated lifecycle

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI;
 from endpoints import router
 from database import Base, engine
-app = FastAPI()
-app.include_router(router,prefix="/api")
-@app.on_event("startup")
-def create_tables():  
-    # Base.metadata.drop_all(bind=engine) #Use this line to drop preexisting tables.
-    Base.metadata.create_all(bind=engine) 
+from contextlib import asynccontextmanager
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # This is startup code that helps to create tables in the database when the application starts. It ensures that the necessary tables are set up before handling any requests.
+    Base.metadata.create_all(bind=engine)
     print("Tables created successfully!")
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router, prefix="/api")
